38329419762101cc9f35556a5e16259815a6d6df
Christophe Van Gysel <cvangysel@uva.nl>
2016-12-05 12:25:13 +0100

    Fixes bug in setup script pertaining to empty arguments being pass to the linker.

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ pyndri_ext = Extension(
     'pyndri_ext',
     sources=['src/pyndri.cpp'],
     libraries=['indri', 'z', 'pthread', 'm'],
-    library_dirs=os.environ.get('LD_LIBRARY_PATH', '').split(':'),
-    define_macros=[('P_NEEDS_GNU_CXX_NAMESPACE', '1')],
+    library_dirs=list(
+        filter(len, os.environ.get('LD_LIBRARY_PATH', '').split(':'))),
+    define_macros=[('_GLIBCXX_USE_CXX11_ABI', '0'),
+                   ('P_NEEDS_GNU_CXX_NAMESPACE', '1')],
     undef_macros=['NDEBUG'])
 
 setup(name='pyndri',
