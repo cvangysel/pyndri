10864dabfec2765bb094465af91cce4a776e9427
Christophe Van Gysel <cvangysel@uva.nl>
2016-12-08 14:44:03 +0100

    Adds minimum/maximum document length statistics.

--- examples/statistics.py
+++ examples/statistics.py
@@ -16,6 +16,9 @@ num_documents = 0
 mean = 0.0
 M2 = 0.0
 
+min_ = np.inf
+max_ = -np.inf
+
 lengths = []
 
 for document_id in range(index.document_base(), index.maximum_document()):
@@ -26,6 +29,9 @@ for document_id in range(index.document_base(), index.maximum_document()):
     delta = x - mean
     mean += delta / num_documents
     M2 += delta * (x - mean)
+
+    min_ = min(min_, x)
+    max_ = max(max_, x)
 
 std = np.sqrt(M2 / (num_documents - 1))
 
@@ -38,6 +44,8 @@ print('export {}NUM={}'.format(prefix, num_documents))
 print('export {}LENGTH_MEDIAN={}'.format(prefix, median))
 print('export {}LENGTH_MODE={}'.format(prefix, mode.mode[0]))
 print('export {}LENGTH_MEAN={}'.format(prefix, mean))
+print('export {}LENGTH_MIN={}'.format(prefix, min_))
+print('export {}LENGTH_MAX={}'.format(prefix, max_))
 print('export {}LENGTH_STD={}'.format(prefix, std))
 print('export {}TOTAL_TERMS={}'.format(prefix, index.total_terms()))
 print('export {}UNIQUE_TERMS={}'.format(prefix, index.unique_terms()))
