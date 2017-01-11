46af3d266315d9fb0ec59a1e4832c494b5e2a241
Mats Willemsen <mats.willemsen@gmail.com>
2017-01-11 17:33:35 +0100

    Modified Extension compiler flags (should be in a makefile)

--- setup.py
+++ setup.py
@@ -1,15 +1,31 @@
 import os
 #from distutils.core import setup, Extension
 from setuptools import setup, Extension
+import pkg_resources
 
-pyndri_ext = Extension(
-    'pyndri_ext',
-    sources=['src/pyndri.cpp'],
-    extra_compile_args=['-std=c++11'],
-    libraries=['indri', 'z', 'pthread', 'm'],
-    library_dirs=list(
-        filter(len, os.environ.get('LD_LIBRARY_PATH', '').split(':'))),
-   undef_macros=['NDEBUG'])
+platform = pkg_resources.get_platform()
+
+if 'macosx' in platform:
+    pyndri_ext = Extension(
+        'pyndri_ext',
+        sources=['src/pyndri.cpp'],
+        extra_compile_args=['-std=c++11'],
+        libraries=['indri', 'z', 'pthread', 'm'],
+        library_dirs=list(
+            filter(len, os.environ.get('LD_LIBRARY_PATH', '').split(':'))),
+        undef_macros=['NDEBUG'])
+
+else:
+     pyndri_ext = Extension(
+        'pyndri_ext',
+        sources=['src/pyndri.cpp'],
+        libraries=['indri', 'z', 'pthread', 'm'],
+        library_dirs=list(
+            filter(len, os.environ.get('LD_LIBRARY_PATH', '').split(':'))),
+        define_macros=[('_GLIBCXX_USE_CXX11_ABI', '0'),
+                    ('P_NEEDS_GNU_CXX_NAMESPACE', '1')],
+        undef_macros=['NDEBUG'])   
+
 
 setup(name='pyndri',
       version='1.0',
